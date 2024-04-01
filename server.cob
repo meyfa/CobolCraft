0013333d379c195b166e58681cba94a7b38df990
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2024-04-02 00:35:47 +0200

    refactor: Move socket functions to separate source file

--- server.cob
+++ server.cob
@@ -4,8 +4,8 @@ PROGRAM-ID. server.
 DATA DIVISION.
 WORKING-STORAGE SECTION.
 01 PORT             PIC X(5) VALUE "25565".
-01 HNDL             PIC X(4).
 01 LISTEN           PIC X(4).
+01 HNDL             PIC X(4).
 01 ERRNO            PIC 9(3) VALUE 0.
 *> State of the player (0 = handshake, 1 = status, 2 = login, 3 = play, 255 = disconnect)
 01 CLIENT-STATE     PIC 9(3) VALUE 0.
@@ -14,19 +14,19 @@ PROCEDURE DIVISION.
 
 Main.
     DISPLAY "Starting server...".
-    CALL "CBL_GC_SOCKET" USING "00" PORT LISTEN GIVING ERRNO.
+    CALL "Socket-Listen" USING PORT LISTEN ERRNO.
     PERFORM HandleError.
 
 AcceptConnection.
     DISPLAY "Waiting for client..."
-    CALL "CBL_GC_SOCKET" USING "07" LISTEN HNDL GIVING ERRNO.
+    CALL "Socket-Accept" USING LISTEN HNDL ERRNO.
     PERFORM HandleError.
 
     MOVE 0 TO CLIENT-STATE.
     PERFORM ReceivePacket UNTIL CLIENT-STATE = 255.
 
     DISPLAY "Disconnecting..."
-    CALL "CBL_GC_SOCKET" USING "06" HNDL GIVING ERRNO.
+    CALL "Socket-Close" USING HNDL ERRNO.
     PERFORM HandleError.
 
     GO TO AcceptConnection.
@@ -42,7 +42,6 @@ ReceivePacket SECTION.
 HandleError SECTION.
     IF ERRNO NOT = 0 THEN
         DISPLAY "Error: " ERRNO
-        CALL "CBL_GC_SOCKET" USING "99" GIVING ERRNO
         STOP RUN
     END-IF.
 
