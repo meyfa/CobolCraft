cd2006c74666fc3093809c0f37662f2c976a967d
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2024-04-04 00:55:53 +0200

    fix: Support encoding negative long and VarInt (#12)

--- src/write.cob
+++ src/write.cob
@@ -20,27 +20,27 @@ PROCEDURE DIVISION USING BY REFERENCE LK-HNDL LK-WRITE-COUNT LK-VALUE LK-ERRNO.
 END PROGRAM Write-Raw.
 
 *> --- Encode-VarInt ---
-*> Encode an integer to a VarInt and store it in a buffer.
-*> TODO: support signed integers
+*> Encode a signed 32-bit integer to a VarInt and store it in a buffer.
 IDENTIFICATION DIVISION.
 PROGRAM-ID. Encode-VarInt.
 
 DATA DIVISION.
 LOCAL-STORAGE SECTION.
-    01 CURRENT-VALUE        PIC 9(10)   VALUE 0.
-    01 CURRENT-VALUE-BITS   PIC 9(3)    VALUE 0.
+    01 CURRENT-VALUE        PIC 9(10).
+    01 CURRENT-VALUE-BITS   PIC 9(3).
 LINKAGE SECTION.
-    01 LK-VALUE-IN          PIC 9(10).
+    01 LK-VALUE-IN          PIC S9(10).
     01 LK-VALUE-OUT         PIC X(5).
     01 LK-OUT-LENGTH        PIC 9(5).
 
 PROCEDURE DIVISION USING BY REFERENCE LK-VALUE-IN LK-VALUE-OUT LK-OUT-LENGTH.
-    IF LK-VALUE-IN = 0
-        MOVE FUNCTION CHAR(1) TO LK-VALUE-OUT(1:1)
-        MOVE 1 TO LK-OUT-LENGTH
-        EXIT PROGRAM
-    END-IF
     MOVE LK-VALUE-IN TO CURRENT-VALUE
+    *> If the number is negative, we need to take the two's complement.
+    *> This is done by computing 2^32 - |x|, where x is the input number.
+    IF LK-VALUE-IN < 0
+        COMPUTE CURRENT-VALUE = 4294967296 - CURRENT-VALUE
+    END-IF
+    *> Perform the conversion to VarInt
     MOVE 0 TO LK-OUT-LENGTH
     PERFORM UNTIL EXIT
         ADD 1 TO LK-OUT-LENGTH
@@ -56,8 +56,7 @@ PROCEDURE DIVISION USING BY REFERENCE LK-VALUE-IN LK-VALUE-OUT LK-OUT-LENGTH.
 END PROGRAM Encode-VarInt.
 
 *> --- Encode-Long ---
-*> Encode a 64-bit integer as 8 bytes (big-endian) and store it in a buffer.
-*> TODO: support signed longs
+*> Encode a 64-bit integer and store it in a buffer (big-endian).
 IDENTIFICATION DIVISION.
 PROGRAM-ID. Encode-Long.
 
@@ -67,16 +66,22 @@ LOCAL-STORAGE SECTION.
     01 CURRENT-VALUE-BYTE   PIC 9(3).
     01 I                    PIC 9(3).
 LINKAGE SECTION.
-    01 LK-VALUE-IN          PIC 9(20).
+    01 LK-VALUE-IN          PIC S9(20).
     01 LK-VALUE-OUT         PIC X(8).
     01 LK-OUT-LENGTH        PIC 9(5).
 
 PROCEDURE DIVISION USING BY REFERENCE LK-VALUE-IN LK-VALUE-OUT LK-OUT-LENGTH.
     MOVE LK-VALUE-IN TO CURRENT-VALUE
+    *> If the number is negative, we need to take the two's complement.
+    *> This is done by computing 2^64 - |x|, where x is the input number.
+    IF LK-VALUE-IN < 0
+        COMPUTE CURRENT-VALUE = 18446744073709551616 - CURRENT-VALUE
+    END-IF
+    *> Perform the conversion to bytes
     MOVE 8 TO LK-OUT-LENGTH
     PERFORM VARYING I FROM 1 BY 1 UNTIL I > LK-OUT-LENGTH
-        DIVIDE CURRENT-VALUE BY 256 GIVING CURRENT-VALUE-BYTE REMAINDER CURRENT-VALUE
-        MOVE FUNCTION CHAR(CURRENT-VALUE-BYTE + 1) TO LK-VALUE-OUT(I:1)
+        DIVIDE CURRENT-VALUE BY 256 GIVING CURRENT-VALUE REMAINDER CURRENT-VALUE-BYTE
+        MOVE FUNCTION CHAR(CURRENT-VALUE-BYTE + 1) TO LK-VALUE-OUT(LK-OUT-LENGTH + 1 - I:1)
     END-PERFORM.
 
 END PROGRAM Encode-Long.
