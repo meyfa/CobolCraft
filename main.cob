9293ddfe0d278f6dcadbd2aeebf3f00277b1ced2
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2024-04-05 13:48:07 +0200

    fix: Use binary data types for better performance (#20)

--- main.cob
+++ main.cob
@@ -5,7 +5,7 @@ DATA DIVISION.
 WORKING-STORAGE SECTION.
     01 SERVER-CONFIG.
         02 PORT                 PIC X(5)    VALUE "25565".
-        02 WHITELIST-ENABLE     PIC 9(1)    VALUE 0.
+        02 WHITELIST-ENABLE     BINARY-CHAR VALUE 0.
         02 WHITELIST-PLAYER     PIC X(16)   VALUE "Notch".
         02 MOTD                 PIC X(64)   VALUE "CobolCraft".
 
