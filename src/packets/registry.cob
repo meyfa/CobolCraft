b088c3e83fa020a8799c0f55cd334ece7c8d7ae4
meyfa <3982806+meyfa@users.noreply.github.com>
2024-05-02 01:09:42 +0200

    fix: Store packet sending error until next tick
    This patch adds a field to the client definition to store a possible
    sending error. If this field is populated, no further writes will be made
    to the socket, and the client will be disconnected on the next tick. It is
    automatically populated by the "SendPacket" program.
    
    There are two large advantages to this method of error handling:
    - If packet sending is part of a larger algorithm, such as block
        placement, there no longer need to be any precautions in place to
        protect the algorithm as a whole from failing when a packet fails.
    - Sending packets becomes much simpler overall, no longer
        requiring a client handle or a path to propagate errors, but only a
        widely-available client ID. This makes it easy to integrate into parts
        of the codebase such as "item use" handlers, where it would have
        previously caused too much complexity.

--- src/packets/registry.cob
+++ src/packets/registry.cob
@@ -12,15 +12,23 @@ FILE SECTION.
     FD FD-REGISTRY-BLOB.
         01 REGISTRY-BLOB-REC    PIC X(64).
 WORKING-STORAGE SECTION.
+    COPY DD-CLIENTS.
+    01 HNDL                     PIC X(4).
+    01 ERRNO            PIC 9(3).
     01 HEX              PIC X(64).
     01 HEXLEN           BINARY-LONG UNSIGNED.
     01 BUFFER           PIC X(32).
     01 BUFFERLEN        BINARY-LONG UNSIGNED.
 LINKAGE SECTION.
-    01 LK-HNDL          PIC X(4).
-    01 LK-ERRNO         PIC 9(3).
+    01 LK-CLIENT        BINARY-LONG UNSIGNED.
 
-PROCEDURE DIVISION USING LK-HNDL LK-ERRNO.
+PROCEDURE DIVISION USING LK-CLIENT.
+    *> Don't send packet if the client is already in an error state. It will be disconnected on the next tick.
+    IF CLIENT-ERRNO-SEND(LK-CLIENT) NOT = 0
+        EXIT PROGRAM
+    END-IF
+    MOVE CLIENT-HNDL(LK-CLIENT) TO HNDL
+
     OPEN INPUT FD-REGISTRY-BLOB
     MOVE 64 TO HEXLEN
     PERFORM UNTIL HEXLEN = 0
@@ -30,13 +38,15 @@ PROCEDURE DIVISION USING LK-HNDL LK-ERRNO.
                 MOVE 0 TO HEXLEN
             NOT AT END
                 CALL "DecodeHexString" USING HEX HEXLEN BUFFER BUFFERLEN
-                CALL "Socket-Write" USING LK-HNDL LK-ERRNO BUFFERLEN BUFFER
-                IF LK-ERRNO NOT = 0
+                CALL "Socket-Write" USING HNDL ERRNO BUFFERLEN BUFFER
+                IF ERRNO NOT = 0
                     MOVE 0 TO HEXLEN
+                    MOVE ERRNO TO CLIENT-ERRNO-SEND(LK-CLIENT)
                 END-IF
         END-READ
     END-PERFORM
     CLOSE FD-REGISTRY-BLOB
+
     GOBACK.
 
 END PROGRAM SendPacket-Registry.
