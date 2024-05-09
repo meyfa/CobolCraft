d96370b93b149807a36eff01b733e884bb4321a4
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2024-05-09 14:54:00 +0200

    fix: Implement float/double coding without C++ (#162)
    Since GnuCOBOL represents variables of type FLOAT-SHORT and
    FLOAT-LONG in memory as IEEE-754, there is really no need to use
    C++ to perform the float-to-byte conversion. Instead, we can directly
    re-interpret the data as PIC X and use REVERSE to obtain a big-endian
    representation.
    
    Not only is this the nicer way of doing it, but it leads to roughly 15%
    performance gains for encode/decode microbenchmarks.

--- cpp/cobolcraft_util.cpp
+++ cpp/cobolcraft_util.cpp
@@ -43,88 +43,6 @@ static int system_time_millis(char *p1)
     return 0;
 }
 
-static int double_get_bytes(char *in, char *out)
-{
-    if (!in || !out)
-    {
-        return ERRNO_PARAMS;
-    }
-
-    double *out_double = (double *)out;
-    *out_double = *((double *)in);
-
-    // convert to big-endian
-    for (unsigned int i = 0; i < 4; ++i)
-    {
-        char temp = out[i];
-        out[i] = out[7 - i];
-        out[7 - i] = temp;
-    }
-
-    return 0;
-}
-
-static int double_from_bytes(char *in, char *out)
-{
-    if (!in || !out)
-    {
-        return ERRNO_PARAMS;
-    }
-
-    // convert to little-endian
-    static char in_copy[8];
-    for (unsigned int i = 0; i < 8; ++i)
-    {
-        in_copy[i] = in[7 - i];
-    }
-
-    double *out_double = (double *)out;
-    *out_double = *((double *)in_copy);
-
-    return 0;
-}
-
-static int float_get_bytes(char *in, char *out)
-{
-    if (!in || !out)
-    {
-        return ERRNO_PARAMS;
-    }
-
-    float *out_float = (float *)out;
-    *out_float = *((float *)in);
-
-    // convert to big-endian
-    for (unsigned int i = 0; i < 2; ++i)
-    {
-        char temp = out[i];
-        out[i] = out[3 - i];
-        out[3 - i] = temp;
-    }
-
-    return 0;
-}
-
-static int float_from_bytes(char *in, char *out)
-{
-    if (!in || !out)
-    {
-        return ERRNO_PARAMS;
-    }
-
-    // convert to little-endian
-    static char in_copy[4];
-    for (unsigned int i = 0; i < 4; ++i)
-    {
-        in_copy[i] = in[3 - i];
-    }
-
-    float *out_float = (float *)out;
-    *out_float = *((float *)in_copy);
-
-    return 0;
-}
-
 static int console_set_nonblock()
 {
     // set stdin to non-blocking mode
@@ -216,22 +134,6 @@ extern "C" int COBOLCRAFT_UTIL(char *p_code, char *p1, char *p2)
     case 1:
         signal(SIGPIPE, SIG_IGN);
         return 0;
-
-    // Convert IEEE-754 double-precision to byte array (big-endian).
-    case 2:
-        return double_get_bytes(p1, p2);
-
-    // Convert byte array to IEEE-754 double-precision (big-endian).
-    case 3:
-        return double_from_bytes(p1, p2);
-
-    // Convert IEEE-754 single-precision to byte array (big-endian).
-    case 4:
-        return float_get_bytes(p1, p2);
-
-    // Convert byte array to IEEE-754 single-precision (big-endian).
-    case 5:
-        return float_from_bytes(p1, p2);
 
     // Setup stdin for non-blocking read.
     case 6:
