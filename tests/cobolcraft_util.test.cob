651ac3d0aea6b72f4d54e409b3919f8d89d74a5c
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2024-05-11 00:36:43 +0200

    test: Add tests for gzip compression (#168)

--- tests/cobolcraft_util.test.cob
+++ tests/cobolcraft_util.test.cob
@@ -5,6 +5,8 @@ PROGRAM-ID. Test-Util.
 PROCEDURE DIVISION.
     DISPLAY "Test: cobolcraft_util.cpp"
     CALL "Test-LeadingZeros32"
+    CALL "Test-GzipCompress"
+    CALL "Test-GzipDecompress"
     GOBACK.
 
     *> --- Test: Test-LeadingZeros32 ---
@@ -67,5 +69,67 @@ PROCEDURE DIVISION.
         GOBACK.
 
     END PROGRAM Test-LeadingZeros32.
+
+    *> --- Test: Test-GzipCompress ---
+    IDENTIFICATION DIVISION.
+    PROGRAM-ID. Test-GzipCompress.
+
+    DATA DIVISION.
+    WORKING-STORAGE SECTION.
+        01 INPUT-BUFFER         PIC X(100).
+        01 OUTPUT-BUFFER        PIC X(100).
+        01 INPUT-LENGTH         BINARY-LONG UNSIGNED.
+        01 OUTPUT-LENGTH        BINARY-LONG UNSIGNED.
+        01 ERRNO                BINARY-LONG UNSIGNED.
+
+    PROCEDURE DIVISION.
+        DISPLAY "  Test: Test-GzipCompress".
+    Basic.
+        DISPLAY "    Case: Basic - " WITH NO ADVANCING
+        MOVE "Hello, World!" TO INPUT-BUFFER
+        MOVE 13 TO INPUT-LENGTH
+        MOVE SPACES TO OUTPUT-BUFFER
+        MOVE LENGTH OF OUTPUT-BUFFER TO OUTPUT-LENGTH
+        CALL "GzipCompress" USING INPUT-BUFFER INPUT-LENGTH OUTPUT-BUFFER OUTPUT-LENGTH GIVING ERRNO
+        IF ERRNO = 0 AND OUTPUT-LENGTH = 33 AND OUTPUT-BUFFER(1:33) = X"1F8B0800000000000003F348CDC9C9D75108CF2FCA49510400D0C34AEC0D000000"
+            DISPLAY "PASS"
+        ELSE
+            DISPLAY "FAIL"
+        END-IF.
+
+        GOBACK.
+
+    END PROGRAM Test-GzipCompress.
+
+    *> --- Test: Test-GzipDecompress ---
+    IDENTIFICATION DIVISION.
+    PROGRAM-ID. Test-GzipDecompress.
+
+    DATA DIVISION.
+    WORKING-STORAGE SECTION.
+        01 INPUT-BUFFER         PIC X(100).
+        01 OUTPUT-BUFFER        PIC X(100).
+        01 INPUT-LENGTH         BINARY-LONG UNSIGNED.
+        01 OUTPUT-LENGTH        BINARY-LONG UNSIGNED.
+        01 ERRNO                BINARY-LONG UNSIGNED.
+
+    PROCEDURE DIVISION.
+        DISPLAY "  Test: Test-GzipDecompress".
+    Basic.
+        DISPLAY "    Case: Basic - " WITH NO ADVANCING
+        MOVE X"1F8B0800000000000003F348CDC9C9D75108CF2FCA49510400D0C34AEC0D000000" TO INPUT-BUFFER
+        MOVE 33 TO INPUT-LENGTH
+        MOVE SPACES TO OUTPUT-BUFFER
+        MOVE LENGTH OF OUTPUT-BUFFER TO OUTPUT-LENGTH
+        CALL "GzipDecompress" USING INPUT-BUFFER INPUT-LENGTH OUTPUT-BUFFER OUTPUT-LENGTH GIVING ERRNO
+        IF ERRNO = 0 AND OUTPUT-LENGTH = 13 AND OUTPUT-BUFFER(1:13) = "Hello, World!"
+            DISPLAY "PASS"
+        ELSE
+            DISPLAY "FAIL"
+        END-IF.
+
+        GOBACK.
+
+    END PROGRAM Test-GzipDecompress.
 
 END PROGRAM Test-Util.
