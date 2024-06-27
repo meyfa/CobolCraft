89f9cb0b77fab2ab1a251a6dda6438610d3531a5
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2024-06-28 01:26:22 +0200

    fix: Obtain dimension_type and chat_type from registry (#199)
    Continuation of #195 to make use of the data that is now available
    for chat and dimension types.
    
    Note: For some reason, the chat type needs 1 added to it. This was
    already the case when using the blob, however. It seems to have
    changed between 1.20.6 and 1.21. Should keep an eye on this packet.

--- src/packets/player-chat.cob
+++ src/packets/player-chat.cob
@@ -3,20 +3,22 @@ PROGRAM-ID. SendPacket-PlayerChat.
 
 DATA DIVISION.
 WORKING-STORAGE SECTION.
-    01 PACKET-ID        BINARY-LONG             VALUE H'39'.
+    01 PACKET-ID                    BINARY-LONG                 VALUE H'39'.
+    01 C-MINECRAFT-CHAT_TYPE        PIC X(19)                   VALUE "minecraft:chat_type".
+    01 C-MINECRAFT-CHAT             PIC X(14)                   VALUE "minecraft:chat".
     *> temporary data used during encoding
-    01 UINT16           BINARY-SHORT UNSIGNED.
-    01 INT32            BINARY-LONG.
+    01 UINT16                       BINARY-LONG-LONG UNSIGNED.
+    01 INT32                        BINARY-LONG.
     *> buffer used to store the packet data
-    01 PAYLOAD          PIC X(64000).
-    01 PAYLOADPOS       BINARY-LONG UNSIGNED.
-    01 PAYLOADLEN       BINARY-LONG UNSIGNED.
+    01 PAYLOAD                      PIC X(64000).
+    01 PAYLOADPOS                   BINARY-LONG UNSIGNED.
+    01 PAYLOADLEN                   BINARY-LONG UNSIGNED.
 LINKAGE SECTION.
-    01 LK-CLIENT        BINARY-LONG UNSIGNED.
-    01 LK-SENDER-UUID   PIC X(16).
-    01 LK-SENDER-NAME   PIC X ANY LENGTH.
-    01 LK-MESSAGE       PIC X ANY LENGTH.
-    01 LK-MESSAGE-LEN   BINARY-LONG UNSIGNED.
+    01 LK-CLIENT                    BINARY-LONG UNSIGNED.
+    01 LK-SENDER-UUID               PIC X(16).
+    01 LK-SENDER-NAME               PIC X ANY LENGTH.
+    01 LK-MESSAGE                   PIC X ANY LENGTH.
+    01 LK-MESSAGE-LEN               BINARY-LONG UNSIGNED.
 
 PROCEDURE DIVISION USING LK-CLIENT LK-SENDER-UUID LK-SENDER-NAME LK-MESSAGE LK-MESSAGE-LEN.
     MOVE 1 TO PAYLOADPOS
@@ -63,9 +65,9 @@ PROCEDURE DIVISION USING LK-CLIENT LK-SENDER-UUID LK-SENDER-NAME LK-MESSAGE LK-M
     *> --- chat formatting ---
 
     *> chat type
-    *> TODO: This is a reference to the "minecraft:chat_type" registry. Get the correct value from the registry.
-    MOVE X"01" TO PAYLOAD(PAYLOADPOS:1)
-    ADD 1 TO PAYLOADPOS
+    CALL "Registries-Get-EntryId" USING C-MINECRAFT-CHAT_TYPE C-MINECRAFT-CHAT INT32
+    ADD 1 TO INT32
+    CALL "Encode-VarInt" USING INT32 PAYLOAD PAYLOADPOS
 
     *> sender name (NBT string tag)
     MOVE X"08" TO PAYLOAD(PAYLOADPOS:1)
