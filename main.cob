f591a6b91a3532f93e02c6393797bb7e308d8012
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2024-12-26 13:24:59 +0100

    feat: Add a persistent JSON file-based whitelist
    The whitelist can now support up to 100 players. It is stored in a
    whitelist.json file in the server directory, using the same format as the
    official server to be drop-in compatible.
    
    Console and in-game commands are also added to reload the whitelist,
    list whitelisted players, and add/remove players.

--- main.cob
+++ main.cob
@@ -5,8 +5,8 @@ DATA DIVISION.
 WORKING-STORAGE SECTION.
     01 SERVER-CONFIG.
         02 PORT                 PIC X(5)    VALUE "25565".
+        *> To configure the whitelist, use console/in-game commands or edit whitelist.json.
         02 WHITELIST-ENABLE     BINARY-CHAR VALUE 0.
-        02 WHITELIST-PLAYER     PIC X(16)   VALUE "Notch".
         02 MOTD                 PIC X(64)   VALUE "CobolCraft".
 
 PROCEDURE DIVISION.
