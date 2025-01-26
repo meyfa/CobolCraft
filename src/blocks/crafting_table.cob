meyfa/CobolCraft

commit a54a47912bfd2fe6faad376758524c0ac70fabd6

feat: Support moving items around in other inventories (#310)
Implements support for clicking on slots within the crafting table GUI, so that
items can be moved between all of the displayed slots, and this is reflected in
the player's main inventory as well. When the crafting table GUI is opened, the
initial state of all slots is sent to the player via a new "window sync" callback.
Translating slot indices to their storage locations is done via a "set slot"
callback. When the GUI is closed, crafting inputs as well as the mouse item
are moved back to the player inventory if possible.

Note that crafting is not implemented here, but this is certainly a
prerequisite :)

src/blocks/crafting_table.cob
@@ -29,6 +29,7 @@ PROCEDURE DIVISION.
     DATA DIVISION.
     WORKING-STORAGE SECTION.
         COPY DD-PLAYERS.
+        01 WINDOW-SYNC-PTR              PROGRAM-POINTER.
     LINKAGE SECTION.
         COPY DD-CALLBACK-BLOCK-INTERACT.
 
@@ -37,9 +38,11 @@ PROCEDURE DIVISION.
 
         ADD 1 TO PLAYER-WINDOW-ID(LK-PLAYER)
         MOVE WINDOW-TYPE-CRAFTING TO PLAYER-WINDOW-TYPE(LK-PLAYER)
+        CALL "SendPacket-OpenScreen" USING PLAYER-CLIENT(LK-PLAYER) PLAYER-WINDOW-ID(LK-PLAYER) PLAYER-WINDOW-TYPE(LK-PLAYER)
 
-        CALL "SendPacket-OpenScreen" USING PLAYER-CLIENT(LK-PLAYER) PLAYER-WINDOW-ID(LK-PLAYER) WINDOW-TYPE-CRAFTING
-        *> TODO container content
+        *> TODO handle callback not found
+        CALL "GetCallback-WindowSync" USING PLAYER-WINDOW-TYPE(LK-PLAYER) WINDOW-SYNC-PTR
+        CALL WINDOW-SYNC-PTR USING LK-PLAYER
 
         GOBACK.
 

src/inventory/window-crafting.cob
@@ -5,7 +5,9 @@ PROGRAM-ID. RegisterWindow-Crafting.
 DATA DIVISION.
 WORKING-STORAGE SECTION.
     01 WINDOW-TYPE                  BINARY-LONG.
+    01 SYNC-PTR                     PROGRAM-POINTER.
     01 CLOSE-PTR                    PROGRAM-POINTER.
+    01 SET-SLOT-PTR                 PROGRAM-POINTER.
 
 PROCEDURE DIVISION.
     CALL "Registries-Get-EntryId" USING "minecraft:menu" "minecraft:crafting" WINDOW-TYPE
@@ -14,23 +16,111 @@ PROCEDURE DIVISION.
         GOBACK
     END-IF
 
+    SET SYNC-PTR TO ENTRY "Callback-Sync"
     SET CLOSE-PTR TO ENTRY "Callback-Close"
+    SET SET-SLOT-PTR TO ENTRY "Callback-SetSlot"
+    CALL "SetCallback-WindowSync" USING WINDOW-TYPE SYNC-PTR
     CALL "SetCallback-WindowClose" USING WINDOW-TYPE CLOSE-PTR
+    CALL "SetCallback-WindowSetSlot" USING WINDOW-TYPE SET-SLOT-PTR
 
     GOBACK.
+
+    *> --- Callback-Sync ---
+    IDENTIFICATION DIVISION.
+    PROGRAM-ID. Callback-Sync.
+
+    DATA DIVISION.
+    WORKING-STORAGE SECTION.
+        COPY DD-PLAYERS.
+        *> https://minecraft.wiki/w/Minecraft_Wiki:Projects/wiki.vg_merge/Inventory#Crafting_table
+        *> 0 = crafting output, 1..9 = crafting grid, 10..36 = player inventory, 37..45 = hotbar
+        01 WINDOW-INVENTORY-LENGTH      BINARY-LONG UNSIGNED    VALUE 46.
+        01 WINDOW-SLOTS.
+            02 WINDOW-SLOT OCCURS 46 TIMES.
+                COPY DD-INVENTORY-SLOT REPLACING LEADING ==PREFIX== BY ==WINDOW==.
+        01 SLOT                         BINARY-LONG UNSIGNED.
+    LINKAGE SECTION.
+        COPY DD-CALLBACK-WINDOW-SYNC.
+
+    PROCEDURE DIVISION USING LK-PLAYER.
+        *> TODO can we avoid the copies?
+        INITIALIZE PLAYER-WINDOW-SLOTS(LK-PLAYER)
+        PERFORM VARYING SLOT FROM 0 BY 1 UNTIL SLOT > 9
+            MOVE PLAYER-WINDOW-SLOT(LK-PLAYER, SLOT + 1) TO WINDOW-SLOT(SLOT + 1)
+        END-PERFORM
+        PERFORM VARYING SLOT FROM 10 BY 1 UNTIL SLOT > 45
+            MOVE PLAYER-INVENTORY-SLOT(LK-PLAYER, SLOT) TO WINDOW-SLOT(SLOT + 1)
+        END-PERFORM
+
+        ADD 1 TO PLAYER-WINDOW-STATE(LK-PLAYER)
+        CALL "SendPacket-SetContainerContent" USING PLAYER-CLIENT(LK-PLAYER) PLAYER-WINDOW-ID(LK-PLAYER) PLAYER-WINDOW-STATE(LK-PLAYER)
+            WINDOW-INVENTORY-LENGTH WINDOW-SLOTS PLAYER-MOUSE-ITEM(LK-PLAYER)
+
+        GOBACK.
+
+    END PROGRAM Callback-Sync.
 
     *> --- Callback-Close ---
     IDENTIFICATION DIVISION.
     PROGRAM-ID. Callback-Close.
 
     DATA DIVISION.
+    WORKING-STORAGE SECTION.
+        COPY DD-PLAYERS.
+        01 SLOT                     BINARY-LONG UNSIGNED.
     LINKAGE SECTION.
         COPY DD-CALLBACK-WINDOW-CLOSE.
 
     PROCEDURE DIVISION USING LK-PLAYER.
-        *> TODO handle carried item (mouse item) - transfer to inventory when closing
+        *> stash the mouse item
+        CALL "Inventory-StoreItem" USING PLAYER-INVENTORY(LK-PLAYER) PLAYER-MOUSE-ITEM(LK-PLAYER)
+        IF PLAYER-MOUSE-SLOT-COUNT(LK-PLAYER) > 0
+            *> TODO drop item
+            CONTINUE
+        END-IF
+
+        *> stash crafting input
+        PERFORM VARYING SLOT FROM 1 BY 1 UNTIL SLOT > 9
+            CALL "Inventory-StoreItem" USING PLAYER-INVENTORY(LK-PLAYER) PLAYER-WINDOW-SLOT(LK-PLAYER, SLOT + 1)
+            IF PLAYER-WINDOW-SLOT(LK-PLAYER, SLOT + 1) > 0
+                *> TODO drop item
+                CONTINUE
+            END-IF
+        END-PERFORM
+
+        CALL "Inventory-SyncPlayerInventory" USING LK-PLAYER
+
         GOBACK.
 
     END PROGRAM Callback-Close.
+
+    *> --- Callback-SetSlot ---
+    IDENTIFICATION DIVISION.
+    PROGRAM-ID. Callback-SetSlot.
+
+    DATA DIVISION.
+    WORKING-STORAGE SECTION.
+        COPY DD-PLAYERS.
+    LINKAGE SECTION.
+        COPY DD-CALLBACK-WINDOW-SET-SLOT.
+
+    PROCEDURE DIVISION USING LK-PLAYER LK-INDEX LK-SLOT.
+        EVALUATE LK-INDEX
+            WHEN 0 *> crafting output
+                *> TODO
+                CONTINUE
+
+            WHEN 1 THRU 9 *> crafting input
+                MOVE LK-SLOT TO PLAYER-WINDOW-SLOT(LK-PLAYER, LK-INDEX + 1)
+
+            WHEN 10 THRU 45 *> player inventory
+                MOVE LK-SLOT TO PLAYER-INVENTORY-SLOT(LK-PLAYER, LK-INDEX)
+
+            WHEN OTHER
+                DISPLAY "Invalid slot number: " LK-INDEX
+        END-EVALUATE
+        GOBACK.
+
+    END PROGRAM Callback-SetSlot.
 
 END PROGRAM RegisterWindow-Crafting.

src/inventory/window-player.cob
@@ -6,12 +6,51 @@ DATA DIVISION.
 WORKING-STORAGE SECTION.
     *> the player inventory window is not one of the registered window types
     01 WINDOW-TYPE                  BINARY-LONG                 VALUE -1.
+    01 SYNC-PTR                     PROGRAM-POINTER.
     01 CLOSE-PTR                    PROGRAM-POINTER.
+    01 SET-SLOT-PTR                 PROGRAM-POINTER.
 
 PROCEDURE DIVISION.
+    SET SYNC-PTR TO ENTRY "Callback-Sync"
     SET CLOSE-PTR TO ENTRY "Callback-Close"
+    SET SET-SLOT-PTR TO ENTRY "Callback-SetSlot"
+    CALL "SetCallback-WindowSync" USING WINDOW-TYPE SYNC-PTR
     CALL "SetCallback-WindowClose" USING WINDOW-TYPE CLOSE-PTR
+    CALL "SetCallback-WindowSetSlot" USING WINDOW-TYPE SET-SLOT-PTR
     GOBACK.
+
+    *> --- Callback-Sync ---
+    IDENTIFICATION DIVISION.
+    PROGRAM-ID. Callback-Sync.
+
+    DATA DIVISION.
+    WORKING-STORAGE SECTION.
+        COPY DD-PLAYERS.
+        01 PLAYER-INVENTORY-LENGTH      BINARY-LONG UNSIGNED        VALUE 46.
+        01 PLAYER-INVENTORY-WINDOW-ID   BINARY-LONG UNSIGNED        VALUE 0.
+        01 EMPTY-SLOT.
+            COPY DD-INVENTORY-SLOT REPLACING LEADING ==PREFIX== BY ==EMPTY==.
+    LINKAGE SECTION.
+        COPY DD-CALLBACK-WINDOW-SYNC.
+
+    PROCEDURE DIVISION USING LK-PLAYER.
+        *> In contrast to other window types, the player inventory could be synced even with another window open.
+
+        ADD 1 TO PLAYER-INVENTORY-STATE(LK-PLAYER)
+        IF PLAYER-WINDOW-ID(LK-PLAYER) = PLAYER-INVENTORY-WINDOW-ID
+            CALL "SendPacket-SetContainerContent" USING PLAYER-CLIENT(LK-PLAYER) PLAYER-INVENTORY-WINDOW-ID
+                PLAYER-INVENTORY-STATE(LK-PLAYER) PLAYER-INVENTORY-LENGTH PLAYER-INVENTORY(LK-PLAYER)
+                PLAYER-MOUSE-ITEM(LK-PLAYER)
+        ELSE
+            *> if the player has a different window open, do not send the mouse item here
+            CALL "SendPacket-SetContainerContent" USING PLAYER-CLIENT(LK-PLAYER) PLAYER-INVENTORY-WINDOW-ID
+                PLAYER-INVENTORY-STATE(LK-PLAYER) PLAYER-INVENTORY-LENGTH PLAYER-INVENTORY(LK-PLAYER)
+                EMPTY-SLOT
+        END-IF
+
+        GOBACK.
+
+    END PROGRAM Callback-Sync.
 
     *> --- Callback-Close ---
     IDENTIFICATION DIVISION.
@@ -20,20 +59,54 @@ PROCEDURE DIVISION.
     DATA DIVISION.
     WORKING-STORAGE SECTION.
         COPY DD-PLAYERS.
+        01 SLOT                     BINARY-LONG UNSIGNED.
     LINKAGE SECTION.
         COPY DD-CALLBACK-WINDOW-CLOSE.
 
     PROCEDURE DIVISION USING LK-PLAYER.
+        *> stash the mouse item
         CALL "Inventory-StoreItem" USING PLAYER-INVENTORY(LK-PLAYER) PLAYER-MOUSE-ITEM(LK-PLAYER)
         IF PLAYER-MOUSE-SLOT-COUNT(LK-PLAYER) > 0
             *> TODO drop item
             CONTINUE
         END-IF
-        ADD 1 TO PLAYER-WINDOW-STATE(LK-PLAYER)
-        CALL "SendPacket-SetContainerContent" USING LK-PLAYER PLAYER-WINDOW-STATE(LK-PLAYER)
-            PLAYER-INVENTORY(LK-PLAYER) PLAYER-MOUSE-ITEM(LK-PLAYER)
+
+        *> stash crafting input
+        PERFORM VARYING SLOT FROM 1 BY 1 UNTIL SLOT > 4
+            CALL "Inventory-StoreItem" USING PLAYER-INVENTORY(LK-PLAYER) PLAYER-INVENTORY-SLOT(LK-PLAYER, SLOT + 1)
+            IF PLAYER-INVENTORY-SLOT-COUNT(LK-PLAYER, SLOT + 1) > 0
+                *> TODO drop item
+                CONTINUE
+            END-IF
+        END-PERFORM
+
+        *> clear crafting output
+        MOVE 0 TO PLAYER-INVENTORY-SLOT-COUNT(LK-PLAYER, 1)
+
+        CALL "Inventory-SyncPlayerInventory" USING LK-PLAYER
+
         GOBACK.
 
     END PROGRAM Callback-Close.
+
+    *> --- Callback-SetSlot ---
+    IDENTIFICATION DIVISION.
+    PROGRAM-ID. Callback-SetSlot.
+
+    DATA DIVISION.
+    WORKING-STORAGE SECTION.
+        COPY DD-PLAYERS.
+    LINKAGE SECTION.
+        COPY DD-CALLBACK-WINDOW-SET-SLOT.
+
+    PROCEDURE DIVISION USING LK-PLAYER LK-INDEX LK-SLOT.
+        IF LK-INDEX >= 0 AND LK-INDEX < 46
+            MOVE LK-SLOT TO PLAYER-INVENTORY-SLOT(LK-PLAYER, LK-INDEX + 1)
+        ELSE
+            DISPLAY "Invalid slot number: " LK-INDEX
+        END-IF
+        GOBACK.
+
+    END PROGRAM Callback-SetSlot.
 
 END PROGRAM RegisterWindow-Player.

src/packets/serverbound/play/container-click.cob
@@ -8,6 +8,8 @@ WORKING-STORAGE SECTION.
     01 PLAYER-ID                BINARY-LONG.
     01 WINDOW-ID                BINARY-LONG.
     01 STATE-ID                 BINARY-LONG.
+    01 SYNC-PTR                 PROGRAM-POINTER.
+    01 SET-SLOT-PTR             PROGRAM-POINTER.
     01 SLOT                     BINARY-SHORT.
     01 BUTTON                   BINARY-CHAR.
     01 MODE-ENUM                BINARY-LONG.
@@ -38,21 +40,33 @@ PROCEDURE DIVISION USING LK-CLIENT LK-BUFFER LK-OFFSET.
     *> TODO We currently accept the client's changed slot data as correct, but we should really compute it ourselves
     *>      based on slot/button/mode and then check if it matches the client's data.
 
-    *> TOOD implement containers other than inventory
-    IF WINDOW-ID NOT = 0
-        PERFORM SyncInventory
+    *> ignore clicks in windows other than the current one
+    IF WINDOW-ID NOT = PLAYER-WINDOW-ID(PLAYER-ID)
+        GOBACK
+    END-IF
+
+    CALL "GetCallback-WindowSync" USING PLAYER-WINDOW-TYPE(PLAYER-ID) SYNC-PTR
+    CALL "GetCallback-WindowSetSlot" USING PLAYER-WINDOW-TYPE(PLAYER-ID) SET-SLOT-PTR
+    IF SYNC-PTR = NULL OR SET-SLOT-PTR = NULL
+        DISPLAY "RecvPacket-ContainerClick: Unable to handle window type " PLAYER-WINDOW-TYPE(PLAYER-ID)
         GOBACK
     END-IF
 
     *> sync client if state ID differs from last sent
-    IF STATE-ID NOT = PLAYER-WINDOW-STATE(PLAYER-ID)
-        PERFORM SyncInventory
+    IF (WINDOW-ID = 0 AND STATE-ID NOT = PLAYER-INVENTORY-STATE(PLAYER-ID)) OR (WINDOW-ID > 0 AND STATE-ID NOT = PLAYER-WINDOW-STATE(PLAYER-ID))
+        CALL SYNC-PTR USING LK-CLIENT
         GOBACK
     END-IF
 
     *> TODO support dropping items
     IF (MODE-ENUM = 0 AND SLOT = -999) OR (MODE-ENUM = 4)
-        PERFORM SyncInventory
+        CALL SYNC-PTR USING LK-CLIENT
+        GOBACK
+    END-IF
+
+    *> TODO handle offhand swap key (F) in containers
+    If WINDOW-ID > 0 AND MODE-ENUM = 2 AND BUTTON = 40
+        CALL "Inventory-SyncPlayerInventory" USING PLAYER-ID
         GOBACK
     END-IF
 
@@ -64,11 +78,7 @@ PROCEDURE DIVISION USING LK-CLIENT LK-BUFFER LK-OFFSET.
     PERFORM CHANGED-SLOT-COUNT TIMES
         CALL "Decode-Short" USING LK-BUFFER LK-OFFSET SLOT-NUMBER
         PERFORM DecodeSlot
-        IF SLOT-NUMBER >= 0 AND SLOT-NUMBER < 46
-            MOVE CLIENT-SLOT TO PLAYER-INVENTORY-SLOT(PLAYER-ID, SLOT-NUMBER + 1)
-        ELSE
-            DISPLAY "Invalid slot number: " SLOT-NUMBER
-        END-IF
+        CALL SET-SLOT-PTR USING PLAYER-ID SLOT-NUMBER CLIENT-SLOT
     END-PERFORM
 
     *> carried item
@@ -76,12 +86,6 @@ PROCEDURE DIVISION USING LK-CLIENT LK-BUFFER LK-OFFSET.
     MOVE CLIENT-SLOT TO PLAYER-MOUSE-ITEM(PLAYER-ID)
 
     GOBACK.
-
-SyncInventory.
-    ADD 1 TO PLAYER-WINDOW-STATE(PLAYER-ID)
-    CALL "SendPacket-SetContainerContent" USING LK-CLIENT PLAYER-WINDOW-STATE(PLAYER-ID)
-        PLAYER-INVENTORY(PLAYER-ID) PLAYER-MOUSE-ITEM(PLAYER-ID)
-    EXIT PARAGRAPH.
 
 DecodeSlot.
     *> TODO deduplicate slot decoding with "set creative slot" packet

src/callbacks.cob
@@ -244,6 +244,48 @@ PROCEDURE DIVISION USING LK-BLOCK-STATE-ID LK-CB-PTR.
 
 END PROGRAM GetCallback-BlockItem.
 
+*> --- SetCallback-WindowSync ---
+IDENTIFICATION DIVISION.
+PROGRAM-ID. SetCallback-WindowSync.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+    COPY DD-CALLBACKS.
+LINKAGE SECTION.
+    01 LK-WINDOW-TYPE               BINARY-LONG.
+    01 LK-CB-PTR                    PROGRAM-POINTER.
+
+PROCEDURE DIVISION USING LK-WINDOW-TYPE LK-CB-PTR.
+    IF LK-WINDOW-TYPE < 0
+        MOVE LK-CB-PTR TO CB-PTR-SYNC-PLAYER
+        GOBACK
+    END-IF
+    MOVE LK-CB-PTR TO CB-PTR-SYNC(LK-WINDOW-TYPE + 1)
+    GOBACK.
+
+END PROGRAM SetCallback-WindowSync.
+
+*> --- GetCallback-WindowSync ---
+IDENTIFICATION DIVISION.
+PROGRAM-ID. GetCallback-WindowSync.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+    COPY DD-CALLBACKS.
+LINKAGE SECTION.
+    01 LK-WINDOW-TYPE               BINARY-LONG.
+    01 LK-CB-PTR                    PROGRAM-POINTER.
+
+PROCEDURE DIVISION USING LK-WINDOW-TYPE LK-CB-PTR.
+    IF LK-WINDOW-TYPE < 0
+        MOVE CB-PTR-SYNC-PLAYER TO LK-CB-PTR
+        GOBACK
+    END-IF
+    MOVE CB-PTR-SYNC(LK-WINDOW-TYPE + 1) TO LK-CB-PTR
+    GOBACK.
+
+END PROGRAM GetCallback-WindowSync.
+
 *> --- SetCallback-WindowClose ---
 IDENTIFICATION DIVISION.
 PROGRAM-ID. SetCallback-WindowClose.
@@ -285,3 +327,45 @@ PROCEDURE DIVISION USING LK-WINDOW-TYPE LK-CB-PTR.
     GOBACK.
 
 END PROGRAM GetCallback-WindowClose.
+
+*> --- SetCallback-WindowSetSlot ---
+IDENTIFICATION DIVISION.
+PROGRAM-ID. SetCallback-WindowSetSlot.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+    COPY DD-CALLBACKS.
+LINKAGE SECTION.
+    01 LK-WINDOW-TYPE               BINARY-LONG.
+    01 LK-CB-PTR                    PROGRAM-POINTER.
+
+PROCEDURE DIVISION USING LK-WINDOW-TYPE LK-CB-PTR.
+    IF LK-WINDOW-TYPE < 0
+        MOVE LK-CB-PTR TO CB-PTR-SET-SLOT-PLAYER
+        GOBACK
+    END-IF
+    MOVE LK-CB-PTR TO CB-PTR-SET-SLOT(LK-WINDOW-TYPE + 1)
+    GOBACK.
+
+END PROGRAM SetCallback-WindowSetSlot.
+
+*> --- GetCallback-WindowSetSlot ---
+IDENTIFICATION DIVISION.
+PROGRAM-ID. GetCallback-WindowSetSlot.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+    COPY DD-CALLBACKS.
+LINKAGE SECTION.
+    01 LK-WINDOW-TYPE               BINARY-LONG.
+    01 LK-CB-PTR                    PROGRAM-POINTER.
+
+PROCEDURE DIVISION USING LK-WINDOW-TYPE LK-CB-PTR.
+    IF LK-WINDOW-TYPE < 0
+        MOVE CB-PTR-SET-SLOT-PLAYER TO LK-CB-PTR
+        GOBACK
+    END-IF
+    MOVE CB-PTR-SET-SLOT(LK-WINDOW-TYPE + 1) TO LK-CB-PTR
+    GOBACK.
+
+END PROGRAM GetCallback-WindowSetSlot.

src/copybooks/DD-CALLBACK-WINDOW-SET-SLOT.cpy
@@ -1,0 +1,6 @@
+*> --- Copybook: callback parameters for setting the item in a window-specific slot ---
+
+01 LK-PLAYER                BINARY-LONG.
+01 LK-INDEX                 BINARY-SHORT.
+01 LK-SLOT.
+    COPY DD-INVENTORY-SLOT REPLACING LEADING ==PREFIX== BY ==LK==.

src/copybooks/DD-CALLBACK-WINDOW-SYNC.cpy
@@ -1,0 +1,3 @@
+*> --- Copybook: callback parameters for opening and synchronizing windows (e.g., inventories) ---
+
+01 LK-PLAYER                BINARY-LONG.

src/copybooks/DD-CALLBACKS.cpy
@@ -23,6 +23,10 @@
 *> The player inventory does not have a window type, and hence must be stored separately.
 01 WINDOW-CALLBACKS EXTERNAL.
     02 CALLBACK OCCURS 100 TIMES.
+        03 CB-PTR-SYNC USAGE PROGRAM-POINTER.
         03 CB-PTR-CLOSE USAGE PROGRAM-POINTER.
+        03 CB-PTR-SET-SLOT USAGE PROGRAM-POINTER.
     02 PLAYER-INVENTORY-CALLBACKS.
+        03 CB-PTR-SYNC-PLAYER USAGE PROGRAM-POINTER.
         03 CB-PTR-CLOSE-PLAYER USAGE PROGRAM-POINTER.
+        03 CB-PTR-SET-SLOT-PLAYER USAGE PROGRAM-POINTER.

commit 3c66a77d557cf8b5362257505bff6502ea2ac5dd

fix: Fix some inventory sync bugs
Fixes the following bugs introduced in #310:

* When the crafting GUI is synced, its slots are always reset
* Items not able to be stashed back into the inventory should be deleted, but
    were not
* Syncing inventories used the client ID instead of player ID, leading to
    problems with multiple concurrent players

src/blocks/crafting_table.cob
@@ -40,6 +40,8 @@ PROCEDURE DIVISION.
         MOVE WINDOW-TYPE-CRAFTING TO PLAYER-WINDOW-TYPE(LK-PLAYER)
         CALL "SendPacket-OpenScreen" USING PLAYER-CLIENT(LK-PLAYER) PLAYER-WINDOW-ID(LK-PLAYER) PLAYER-WINDOW-TYPE(LK-PLAYER)
 
+        INITIALIZE PLAYER-WINDOW-SLOTS(LK-PLAYER)
+
         *> TODO handle callback not found
         CALL "GetCallback-WindowSync" USING PLAYER-WINDOW-TYPE(LK-PLAYER) WINDOW-SYNC-PTR
         CALL WINDOW-SYNC-PTR USING LK-PLAYER

src/inventory/window-crafting.cob
@@ -44,7 +44,6 @@ PROCEDURE DIVISION.
 
     PROCEDURE DIVISION USING LK-PLAYER.
         *> TODO can we avoid the copies?
-        INITIALIZE PLAYER-WINDOW-SLOTS(LK-PLAYER)
         PERFORM VARYING SLOT FROM 0 BY 1 UNTIL SLOT > 9
             MOVE PLAYER-WINDOW-SLOT(LK-PLAYER, SLOT + 1) TO WINDOW-SLOT(SLOT + 1)
         END-PERFORM
@@ -76,7 +75,7 @@ PROCEDURE DIVISION.
         CALL "Inventory-StoreItem" USING PLAYER-INVENTORY(LK-PLAYER) PLAYER-MOUSE-ITEM(LK-PLAYER)
         IF PLAYER-MOUSE-SLOT-COUNT(LK-PLAYER) > 0
             *> TODO drop item
-            CONTINUE
+            MOVE 0 TO PLAYER-MOUSE-SLOT-COUNT(LK-PLAYER)
         END-IF
 
         *> stash crafting input
@@ -84,7 +83,7 @@ PROCEDURE DIVISION.
             CALL "Inventory-StoreItem" USING PLAYER-INVENTORY(LK-PLAYER) PLAYER-WINDOW-SLOT(LK-PLAYER, SLOT + 1)
             IF PLAYER-WINDOW-SLOT(LK-PLAYER, SLOT + 1) > 0
                 *> TODO drop item
-                CONTINUE
+                MOVE 0 TO PLAYER-WINDOW-SLOT(LK-PLAYER, SLOT + 1)
             END-IF
         END-PERFORM
 

src/inventory/window-player.cob
@@ -68,7 +68,7 @@ PROCEDURE DIVISION.
         CALL "Inventory-StoreItem" USING PLAYER-INVENTORY(LK-PLAYER) PLAYER-MOUSE-ITEM(LK-PLAYER)
         IF PLAYER-MOUSE-SLOT-COUNT(LK-PLAYER) > 0
             *> TODO drop item
-            CONTINUE
+            MOVE 0 TO PLAYER-MOUSE-SLOT-COUNT(LK-PLAYER)
         END-IF
 
         *> stash crafting input
@@ -76,7 +76,7 @@ PROCEDURE DIVISION.
             CALL "Inventory-StoreItem" USING PLAYER-INVENTORY(LK-PLAYER) PLAYER-INVENTORY-SLOT(LK-PLAYER, SLOT + 1)
             IF PLAYER-INVENTORY-SLOT-COUNT(LK-PLAYER, SLOT + 1) > 0
                 *> TODO drop item
-                CONTINUE
+                MOVE 0 TO PLAYER-INVENTORY-SLOT-COUNT(LK-PLAYER, SLOT + 1)
             END-IF
         END-PERFORM
 

src/packets/serverbound/play/container-click.cob
@@ -54,13 +54,13 @@ PROCEDURE DIVISION USING LK-CLIENT LK-BUFFER LK-OFFSET.
 
     *> sync client if state ID differs from last sent
     IF (WINDOW-ID = 0 AND STATE-ID NOT = PLAYER-INVENTORY-STATE(PLAYER-ID)) OR (WINDOW-ID > 0 AND STATE-ID NOT = PLAYER-WINDOW-STATE(PLAYER-ID))
-        CALL SYNC-PTR USING LK-CLIENT
+        CALL SYNC-PTR USING PLAYER-ID
         GOBACK
     END-IF
 
     *> TODO support dropping items
     IF (MODE-ENUM = 0 AND SLOT = -999) OR (MODE-ENUM = 4)
-        CALL SYNC-PTR USING LK-CLIENT
+        CALL SYNC-PTR USING PLAYER-ID
         GOBACK
     END-IF
 