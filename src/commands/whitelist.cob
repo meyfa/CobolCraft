893437d1aaf57d2ff41b7b0a9f139d09e989a101
Simon Sobisch <simonsobisch@web.de>
2025-01-26 13:44:30 +0000

    whitelist adjustments
    fixing a reference-out-of-bounds exception in case of "list" command and lots of whitelisted users (max: 100 users * 16 chars = 1600 with buffer being of size 255)
    
    showcasing:
    * STRING ... WITH POINTER (that would also never write out of bounds and would allow an easy ON OVERFLOW EXIT PERFORM)
    * EVALUATE statement, including ALSO
    * mixed static and "if needed" dynamic memory

--- src/commands/whitelist.cob
+++ src/commands/whitelist.cob
@@ -14,26 +14,26 @@ WORKING-STORAGE SECTION.
 
 PROCEDURE DIVISION.
     SET PTR TO ENTRY "Callback-Execute"
-    CALL "RegisterCommand" USING COMMAND-NAME COMMAND-HELP PTR NODE-ROOT
+    CALL "RegisterCommand"      USING COMMAND-NAME COMMAND-HELP PTR NODE-ROOT
 
-    CALL "AddCommandLiteral" USING NODE-ROOT "reload" NODE-OPERATION
+    CALL "AddCommandLiteral"    USING NODE-ROOT "reload" NODE-OPERATION
     CALL "SetCommandExecutable" USING NODE-OPERATION
 
-    CALL "AddCommandLiteral" USING NODE-ROOT "on" NODE-OPERATION
+    CALL "AddCommandLiteral"    USING NODE-ROOT "on" NODE-OPERATION
     CALL "SetCommandExecutable" USING NODE-OPERATION
 
-    CALL "AddCommandLiteral" USING NODE-ROOT "off" NODE-OPERATION
+    CALL "AddCommandLiteral"    USING NODE-ROOT "off" NODE-OPERATION
     CALL "SetCommandExecutable" USING NODE-OPERATION
 
-    CALL "AddCommandLiteral" USING NODE-ROOT "list" NODE-OPERATION
+    CALL "AddCommandLiteral"    USING NODE-ROOT "list" NODE-OPERATION
     CALL "SetCommandExecutable" USING NODE-OPERATION
 
-    CALL "AddCommandLiteral" USING NODE-ROOT "add" NODE-OPERATION
-    CALL "AddCommandArgument" USING NODE-OPERATION "player" CMD-PARSER-GAME-PROFILE OMITTED NODE-ARGUMENT
+    CALL "AddCommandLiteral"    USING NODE-ROOT "add" NODE-OPERATION
+    CALL "AddCommandArgument"   USING NODE-OPERATION "player" CMD-PARSER-GAME-PROFILE OMITTED NODE-ARGUMENT
     CALL "SetCommandExecutable" USING NODE-ARGUMENT
 
-    CALL "AddCommandLiteral" USING NODE-ROOT "remove" NODE-OPERATION
-    CALL "AddCommandArgument" USING NODE-OPERATION "player" CMD-PARSER-GAME-PROFILE OMITTED NODE-ARGUMENT
+    CALL "AddCommandLiteral"    USING NODE-ROOT "remove" NODE-OPERATION
+    CALL "AddCommandArgument"   USING NODE-OPERATION "player" CMD-PARSER-GAME-PROFILE OMITTED NODE-ARGUMENT
     CALL "SetCommandExecutable" USING NODE-ARGUMENT
 
     GOBACK.
@@ -46,9 +46,10 @@ PROCEDURE DIVISION.
     WORKING-STORAGE SECTION.
         COPY DD-WHITELIST.
         COPY DD-SERVER-PROPERTIES.
-        01 BUFFER                   PIC X(255).
+        01 BUFFER                   PIC X(128).
         01 BUFFER-POS               BINARY-LONG UNSIGNED.
-        01 BYTE-COUNT               BINARY-LONG UNSIGNED.
+        01 BUFFER-ADDRESS           USAGE POINTER.
+        01 DYN-BUFFER-LEN           BINARY-LONG UNSIGNED VALUE ZERO.
         01 WHITELIST-INDEX          BINARY-LONG UNSIGNED.
         01 IO-FAILURE               BINARY-CHAR UNSIGNED.
         01 STATE-FAILURE            BINARY-CHAR UNSIGNED.
@@ -58,98 +59,107 @@ PROCEDURE DIVISION.
     LINKAGE SECTION.
         COPY DD-CALLBACK-COMMAND-EXECUTE.
 
+    01  DYN-BUFFER.
+        03  FILLER     PIC X OCCURS UNBOUNDED DEPENDING ON DYN-BUFFER-LEN.
+
     PROCEDURE DIVISION USING LK-CLIENT-ID LK-PARTS LK-PRINT-USAGE.
-        IF LK-PART-COUNT = 2 AND LK-PART-VALUE(2) = "reload"
-            CALL "Whitelist-Read" USING IO-FAILURE
-            PERFORM HandleDiskFailure
-            MOVE "Reloaded the whitelist" TO BUFFER
-            CALL "SendChatMessage" USING LK-CLIENT-ID BUFFER OMITTED
+
+        IF LK-PART-COUNT < 2
+            MOVE 1 TO LK-PRINT-USAGE
             GOBACK
         END-IF
 
-        IF LK-PART-COUNT = 2 AND LK-PART-VALUE(2) = "on"
+        EVALUATE LK-PART-COUNT ALSO LK-PART-VALUE(2)
+
+        WHEN 2 ALSO "reload"
+            CALL "Whitelist-Read" USING IO-FAILURE
+            PERFORM HandleDiskFailure
+            MOVE "Reloaded the whitelist" TO BUFFER
+
+        WHEN 2 ALSO "on"
             MOVE 1 TO SP-WHITELIST-ENABLE
             CALL "ServerProperties-Write" USING IO-FAILURE
             PERFORM HandleDiskFailure
             MOVE "Whitelist enabled" TO BUFFER
-            CALL "SendChatMessage" USING LK-CLIENT-ID BUFFER OMITTED
-            GOBACK
-        END-IF
 
-        IF LK-PART-COUNT = 2 AND LK-PART-VALUE(2) = "off"
+        WHEN 2 ALSO  "off"
             MOVE 0 TO SP-WHITELIST-ENABLE
             CALL "ServerProperties-Write" USING IO-FAILURE
             PERFORM HandleDiskFailure
             MOVE "Whitelist disabled" TO BUFFER
-            CALL "SendChatMessage" USING LK-CLIENT-ID BUFFER OMITTED
-            GOBACK
-        END-IF
 
-        IF LK-PART-COUNT = 2 AND LK-PART-VALUE(2) = "list"
+        WHEN 2 ALSO  "list"
             IF WHITELIST-LENGTH = 0
                 MOVE "There are no whitelisted players" TO BUFFER
-                CALL "SendChatMessage" USING LK-CLIENT-ID BUFFER OMITTED
+            ELSE
+                MOVE WHITELIST-LENGTH TO TEMP-INT64-PIC
+                COMPUTE DYN-BUFFER-LEN = 32 + WHITELIST-LENGTH * (LENGTH OF WHITELIST-NAME (1) + 2)
+                *> overwrite the dynamic buffer's address with a dynamic buffer, if too big
+                IF DYN-BUFFER-LEN > LENGTH OF BUFFER
+                   ALLOCATE DYN-BUFFER-LEN CHARACTERS RETURNING BUFFER-ADDRESS
+                   SET ADDRESS OF DYN-BUFFER TO BUFFER-ADDRESS
+                ELSE
+                   SET ADDRESS OF DYN-BUFFER TO ADDRESS OF BUFFER
+                END-IF
+                MOVE 1 TO BUFFER-POS
+                *> no need to initialize the buffer as we only use it in the places we explicit set
+                STRING "There are " FUNCTION TRIM(TEMP-INT64-PIC, LEADING) " whitelisted player(s): "
+                        FUNCTION TRIM (WHITELIST-NAME(1), TRAILING)
+                        INTO DYN-BUFFER WITH POINTER BUFFER-POS
+                PERFORM WITH TEST AFTER
+                        VARYING WHITELIST-INDEX FROM 2 BY 1 UNTIL WHITELIST-INDEX = WHITELIST-LENGTH
+                    STRING ", " FUNCTION TRIM (WHITELIST-NAME(WHITELIST-INDEX), TRAILING)
+                          INTO DYN-BUFFER WITH POINTER BUFFER-POS
+                          *> note: we ensured that the buffer is always big enough, so no need for ON OVERFLOW here
+                END-PERFORM
+                SUBTRACT 1 FROM BUFFER-POS
+                CALL "SendChatMessage" USING LK-CLIENT-ID DYN-BUFFER OMITTED BUFFER-POS
+                IF DYN-BUFFER-LEN > LENGTH OF BUFFER
+                   FREE BUFFER-ADDRESS
+                END-IF
                 GOBACK
             END-IF
-            MOVE WHITELIST-LENGTH TO TEMP-INT64-PIC
-            INITIALIZE BUFFER
-            STRING "There are " FUNCTION TRIM(TEMP-INT64-PIC) " whitelisted player(s):" INTO BUFFER
-            COMPUTE BUFFER-POS = FUNCTION STORED-CHAR-LENGTH(BUFFER) + 2
-            PERFORM VARYING WHITELIST-INDEX FROM 1 BY 1 UNTIL WHITELIST-INDEX > WHITELIST-LENGTH
-                IF WHITELIST-INDEX > 1
-                    MOVE ", " TO BUFFER(BUFFER-POS:2)
-                    ADD 2 TO BUFFER-POS
-                END-IF
-                MOVE FUNCTION STORED-CHAR-LENGTH(WHITELIST-NAME(WHITELIST-INDEX)) TO BYTE-COUNT
-                MOVE WHITELIST-NAME(WHITELIST-INDEX)(1:BYTE-COUNT) TO BUFFER(BUFFER-POS:BYTE-COUNT)
-                ADD BYTE-COUNT TO BUFFER-POS
-            END-PERFORM
-            CALL "SendChatMessage" USING LK-CLIENT-ID BUFFER OMITTED
-            GOBACK
-        END-IF
 
-        IF LK-PART-COUNT = 3
+        WHEN 3 ALSO "add"
             MOVE LK-PART-VALUE(3) TO TEMP-NAME
             CALL "Players-NameToUUID" USING TEMP-NAME TEMP-UUID
 
-            IF LK-PART-VALUE(2) = "add"
-                CALL "Whitelist-Add" USING TEMP-UUID TEMP-NAME STATE-FAILURE IO-FAILURE
-                PERFORM HandleDiskFailure
-                IF STATE-FAILURE NOT = 0
-                    MOVE "Player is already whitelisted" TO BUFFER
-                ELSE
-                    INITIALIZE BUFFER
-                    STRING "Added " FUNCTION TRIM(TEMP-NAME) " to the whitelist" INTO BUFFER
-                END-IF
-                CALL "SendChatMessage" USING LK-CLIENT-ID BUFFER OMITTED
-                GOBACK
+            INITIALIZE BUFFER
+            CALL "Whitelist-Add" USING TEMP-UUID TEMP-NAME STATE-FAILURE IO-FAILURE
+            PERFORM HandleDiskFailure
+            IF STATE-FAILURE NOT = 0
+                STRING "Player " FUNCTION TRIM(TEMP-NAME) " is already whitelisted" INTO BUFFER
+            ELSE
+                STRING "Added " FUNCTION TRIM(TEMP-NAME) " to the whitelist" INTO BUFFER
             END-IF
 
-            IF LK-PART-VALUE(2) = "remove"
-                CALL "Whitelist-Remove" USING TEMP-UUID TEMP-NAME STATE-FAILURE IO-FAILURE
-                PERFORM HandleDiskFailure
-                IF STATE-FAILURE NOT = 0
-                    MOVE "Player is not whitelisted" TO BUFFER
-                ELSE
-                    INITIALIZE BUFFER
-                    STRING "Removed " FUNCTION TRIM(TEMP-NAME) " from the whitelist" INTO BUFFER
-                END-IF
-                CALL "SendChatMessage" USING LK-CLIENT-ID BUFFER OMITTED
-                GOBACK
+        WHEN 3 ALSO  "remove"
+            MOVE LK-PART-VALUE(3) TO TEMP-NAME
+            CALL "Players-NameToUUID" USING TEMP-NAME TEMP-UUID
+
+            INITIALIZE BUFFER
+            CALL "Whitelist-Remove" USING TEMP-UUID TEMP-NAME STATE-FAILURE IO-FAILURE
+            PERFORM HandleDiskFailure
+            IF STATE-FAILURE NOT = 0
+                STRING "Player " FUNCTION TRIM(TEMP-NAME) " is not whitelisted" INTO BUFFER
+            ELSE
+                STRING "Removed " FUNCTION TRIM(TEMP-NAME) " from the whitelist" INTO BUFFER
             END-IF
-        END-IF
 
-        MOVE 1 TO LK-PRINT-USAGE
+        WHEN OTHER
+            MOVE 1 TO LK-PRINT-USAGE
+            GOBACK
 
+        END-EVALUATE.
+
+        CALL "SendChatMessage" USING LK-CLIENT-ID BUFFER
         GOBACK.
 
     HandleDiskFailure.
         IF IO-FAILURE NOT = 0
-            MOVE "Input/output error" TO BUFFER
-            CALL "SendChatMessage" USING LK-CLIENT-ID BUFFER OMITTED
+            CALL "SendChatMessage" USING LK-CLIENT-ID "Input/output error"
             GOBACK
-        END-IF
-        EXIT PARAGRAPH.
+        END-IF.
 
     END PROGRAM Callback-Execute.
 
