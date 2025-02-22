c6b85d21264aa637f8b838881987551e8b1b3e37
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2025-02-22 13:57:01 +0100

    refactor: Split large files and rethink directory structure (#343)
    * Split `world.cob` into multiple smaller files, as it was over 2000
        lines and becoming hard to manage.
    * Similarly, split `players.cob` into one file for I/O and one for
        player-related logic.
    * Rename `chunks.cob` into `client-chunks.cob` to better reflect its
        purpose and avoid confusion with `world/chunks.cob`.
    * Move everything related to encode/decode into `src/encoding/` to free
        up the top-level `src/` directory.
    * Move `chat.cob` and `coordinates.cob` into `src/util/` as they don't
        contain important functionality.
    * Move test files to reflect the src changes.

---
+++ src/world/chunks.cob
@@ -0,0 +1,309 @@
+*> --- World-AllocateChunk ---
+*> Find a free chunk slot. If a chunk with the given coordinates is present, it is freed first.
+*> All blocks in the chunk are set to air, and the coordinates are set. The chunk is, however, not marked as present.
+IDENTIFICATION DIVISION.
+PROGRAM-ID. World-AllocateChunk.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+    COPY DD-WORLD.
+    COPY DD-CHUNK-REF.
+    01 BIOME-ID                 BINARY-LONG UNSIGNED.
+    01 SECTION-INDEX            BINARY-LONG UNSIGNED.
+LINKAGE SECTION.
+    01 LK-CHUNK-X               BINARY-LONG.
+    01 LK-CHUNK-Z               BINARY-LONG.
+    01 LK-CHUNK-INDEX           BINARY-LONG UNSIGNED.
+
+PROCEDURE DIVISION USING LK-CHUNK-X LK-CHUNK-Z LK-CHUNK-INDEX.
+    *> Check if the chunk is already present and free it if needed
+    PERFORM VARYING LK-CHUNK-INDEX FROM 1 BY 1 UNTIL LK-CHUNK-INDEX > WORLD-CHUNK-COUNT
+        SET ADDRESS OF CHUNK TO WORLD-CHUNK-POINTER(LK-CHUNK-INDEX)
+        IF LK-CHUNK-X = CHUNK-X AND LK-CHUNK-Z = CHUNK-Z
+            CALL "World-FreeChunk" USING LK-CHUNK-INDEX
+            EXIT PERFORM
+        END-IF
+    END-PERFORM
+
+    *> Allocate a new chunk
+    COMPUTE LK-CHUNK-INDEX = WORLD-CHUNK-COUNT + 1
+    IF LK-CHUNK-INDEX >= WORLD-CHUNK-CAPACITY
+        DISPLAY "Warning: Failed to allocate chunk (no capacity)"
+        MOVE 0 TO LK-CHUNK-INDEX
+        GOBACK
+    END-IF
+    ALLOCATE CHUNK
+    SET WORLD-CHUNK-POINTER(LK-CHUNK-INDEX) TO ADDRESS OF CHUNK
+
+    INITIALIZE CHUNK
+    MOVE LK-CHUNK-X TO CHUNK-X
+    MOVE LK-CHUNK-Z TO CHUNK-Z
+    INITIALIZE CHUNK-BLOCK-ENTITY-IDS REPLACING NUMERIC BY -1
+
+    *> Set all sections to the plains biome
+    CALL "Registries-Get-EntryId" USING "minecraft:worldgen/biome" "minecraft:plains" BIOME-ID
+    PERFORM VARYING SECTION-INDEX FROM 1 BY 1 UNTIL SECTION-INDEX > CHUNK-SECTION-COUNT
+        INITIALIZE CHUNK-SECTION-BIOMES(SECTION-INDEX) REPLACING NUMERIC BY BIOME-ID
+    END-PERFORM
+
+    ADD 1 TO WORLD-CHUNK-COUNT
+
+    GOBACK.
+
+END PROGRAM World-AllocateChunk.
+
+*> --- World-FreeChunk ---
+*> Free a chunk slot, properly deallocating any dynamically allocated memory.
+IDENTIFICATION DIVISION.
+PROGRAM-ID. World-FreeChunk.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+    COPY DD-WORLD.
+    COPY DD-CHUNK-REF.
+    COPY DD-CHUNK-ENTITY.
+    01 ENTITY-PTR               POINTER.
+LINKAGE SECTION.
+    01 LK-CHUNK-INDEX           BINARY-LONG UNSIGNED.
+
+PROCEDURE DIVISION USING LK-CHUNK-INDEX.
+    SET ADDRESS OF CHUNK TO WORLD-CHUNK-POINTER(LK-CHUNK-INDEX)
+
+    *> free all entities
+    SET ENTITY-PTR TO CHUNK-ENTITY-LIST
+    PERFORM UNTIL ENTITY-PTR = NULL
+        SET ADDRESS OF ENTITY-LIST TO ENTITY-PTR
+        SET ENTITY-PTR TO ENTITY-LIST-NEXT
+        FREE ENTITY-LIST
+    END-PERFORM
+
+    *> free the chunk itself
+    FREE WORLD-CHUNK-POINTER(LK-CHUNK-INDEX)
+
+    *> move the last slot into the freed slot
+    SET WORLD-CHUNK-POINTER(LK-CHUNK-INDEX) TO WORLD-CHUNK-POINTER(WORLD-CHUNK-COUNT)
+    SUBTRACT 1 FROM WORLD-CHUNK-COUNT
+
+    GOBACK.
+
+END PROGRAM World-FreeChunk.
+
+*> --- World-FindChunkIndex ---
+*> Find a chunk that is present and has the given coordinates.
+IDENTIFICATION DIVISION.
+PROGRAM-ID. World-FindChunkIndex.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+    COPY DD-WORLD.
+    COPY DD-CHUNK-REF.
+LINKAGE SECTION.
+    01 LK-CHUNK-X               BINARY-LONG.
+    01 LK-CHUNK-Z               BINARY-LONG.
+    01 LK-CHUNK-INDEX           BINARY-LONG UNSIGNED.
+
+PROCEDURE DIVISION USING LK-CHUNK-X LK-CHUNK-Z LK-CHUNK-INDEX.
+    PERFORM VARYING LK-CHUNK-INDEX FROM 1 BY 1 UNTIL LK-CHUNK-INDEX > WORLD-CHUNK-COUNT
+        SET ADDRESS OF CHUNK TO WORLD-CHUNK-POINTER(LK-CHUNK-INDEX)
+        IF LK-CHUNK-X = CHUNK-X AND LK-CHUNK-Z = CHUNK-Z
+            GOBACK
+        END-IF
+    END-PERFORM
+    MOVE 0 TO LK-CHUNK-INDEX
+    GOBACK.
+
+END PROGRAM World-FindChunkIndex.
+
+*> --- World-GenerateChunk ---
+IDENTIFICATION DIVISION.
+PROGRAM-ID. World-GenerateChunk.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+    COPY DD-WORLD.
+    COPY DD-CHUNK-REF.
+LOCAL-STORAGE SECTION.
+    01 CHUNK-INDEX              BINARY-LONG UNSIGNED.
+    01 SECTION-INDEX            BINARY-LONG UNSIGNED.
+    01 BLOCK-INDEX              BINARY-LONG UNSIGNED.
+    01 TEMP-INT32               BINARY-LONG.
+LINKAGE SECTION.
+    01 LK-CHUNK-X               BINARY-LONG.
+    01 LK-CHUNK-Z               BINARY-LONG.
+
+PROCEDURE DIVISION USING LK-CHUNK-X LK-CHUNK-Z.
+    CALL "World-AllocateChunk" USING LK-CHUNK-X LK-CHUNK-Z CHUNK-INDEX
+    IF CHUNK-INDEX = 0
+        GOBACK
+    END-IF
+
+    SET ADDRESS OF CHUNK TO WORLD-CHUNK-POINTER(CHUNK-INDEX)
+
+    *> turn all blocks with Y <= 63 (= the bottom 128 blocks = the bottom 8 sections) into stone
+    CALL "Blocks-Get-DefaultStateId" USING "minecraft:stone" TEMP-INT32
+    PERFORM VARYING SECTION-INDEX FROM 1 BY 1 UNTIL SECTION-INDEX > 8
+        PERFORM VARYING BLOCK-INDEX FROM 1 BY 1 UNTIL BLOCK-INDEX > 4096
+            MOVE TEMP-INT32 TO CHUNK-SECTION-BLOCK(SECTION-INDEX, BLOCK-INDEX)
+        END-PERFORM
+        MOVE 4096 TO CHUNK-SECTION-NON-AIR(SECTION-INDEX)
+    END-PERFORM
+
+    *> turn all blocks with Y = 63 (i.e., the top 16x16 blocks) into grass
+    CALL "Blocks-Get-DefaultStateId" USING "minecraft:grass_block" TEMP-INT32
+    MOVE 8 TO SECTION-INDEX
+    COMPUTE BLOCK-INDEX = 4096 - 256 + 1
+    PERFORM 256 TIMES
+        MOVE TEMP-INT32 TO CHUNK-SECTION-BLOCK(SECTION-INDEX, BLOCK-INDEX)
+        *> Note: No need to increment CHUNK-SECTION-NON-AIR, as the section is already full
+        ADD 1 TO BLOCK-INDEX
+    END-PERFORM
+
+    *> chunk needs to be saved
+    MOVE 1 TO CHUNK-DIRTY
+
+    GOBACK.
+
+END PROGRAM World-GenerateChunk.
+
+*> --- World-EnsureChunk ---
+IDENTIFICATION DIVISION.
+PROGRAM-ID. World-EnsureChunk.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+    COPY DD-WORLD.
+    01 IO-FAILURE               BINARY-CHAR UNSIGNED.
+LINKAGE SECTION.
+    01 LK-CHUNK-X               BINARY-LONG.
+    01 LK-CHUNK-Z               BINARY-LONG.
+    01 LK-CHUNK-INDEX           BINARY-LONG UNSIGNED.
+
+PROCEDURE DIVISION USING LK-CHUNK-X LK-CHUNK-Z LK-CHUNK-INDEX.
+    *> attempt to find the chunk
+    CALL "World-FindChunkIndex" USING LK-CHUNK-X LK-CHUNK-Z LK-CHUNK-INDEX
+    IF LK-CHUNK-INDEX > 0
+        GOBACK
+    END-IF
+    *> not found, load or generate
+    CALL "World-LoadChunk" USING LK-CHUNK-X LK-CHUNK-Z IO-FAILURE
+    IF IO-FAILURE NOT = 0
+        MOVE 0 TO IO-FAILURE
+        CALL "World-GenerateChunk" USING LK-CHUNK-X LK-CHUNK-Z
+    END-IF
+    *> find the chunk again
+    CALL "World-FindChunkIndex" USING LK-CHUNK-X LK-CHUNK-Z LK-CHUNK-INDEX
+    GOBACK.
+
+END PROGRAM World-EnsureChunk.
+
+*> --- World-EnsureSpawnChunks ---
+IDENTIFICATION DIVISION.
+PROGRAM-ID. World-EnsureSpawnChunks.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+    COPY DD-WORLD.
+    COPY DD-CHUNK-REF.
+    01 X-START                  BINARY-LONG.
+    01 X-END                    BINARY-LONG.
+    01 Z-START                  BINARY-LONG.
+    01 Z-END                    BINARY-LONG.
+    01 X-POS                    BINARY-LONG.
+    01 Z-POS                    BINARY-LONG.
+    01 CHUNK-INDEX              BINARY-LONG UNSIGNED.
+LINKAGE SECTION.
+    01 LK-SAVE-REQUIRED         BINARY-CHAR UNSIGNED.
+
+PROCEDURE DIVISION USING LK-SAVE-REQUIRED.
+    MOVE 0 TO LK-SAVE-REQUIRED
+
+    COMPUTE X-START ROUNDED MODE IS TOWARD-LESSER = (WORLD-SPAWN-X / 16) - 1
+    COMPUTE Z-START ROUNDED MODE IS TOWARD-LESSER = (WORLD-SPAWN-Z / 16) - 1
+    COMPUTE X-END = X-START + 2
+    COMPUTE Z-END = Z-START + 2
+
+    PERFORM VARYING X-POS FROM X-START BY 1 UNTIL X-POS > X-END
+        PERFORM VARYING Z-POS FROM Z-START BY 1 UNTIL Z-POS > Z-END
+            CALL "World-EnsureChunk" USING X-POS Z-POS CHUNK-INDEX
+            IF CHUNK-INDEX = 0
+                GOBACK
+            END-IF
+            SET ADDRESS OF CHUNK TO WORLD-CHUNK-POINTER(CHUNK-INDEX)
+            IF CHUNK-DIRTY > 0
+                MOVE 1 TO LK-SAVE-REQUIRED
+            END-IF
+        END-PERFORM
+    END-PERFORM
+
+    GOBACK.
+
+END PROGRAM World-EnsureSpawnChunks.
+
+*> --- World-UnloadChunks ---
+IDENTIFICATION DIVISION.
+PROGRAM-ID. World-UnloadChunks.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+    01 CHUNK-INDEX              BINARY-LONG UNSIGNED.
+    01 CHUNK-BLOCK-X            BINARY-LONG.
+    01 CHUNK-BLOCK-Z            BINARY-LONG.
+    01 SPAWN-X-START            BINARY-LONG.
+    01 SPAWN-X-END              BINARY-LONG.
+    01 SPAWN-Z-START            BINARY-LONG.
+    01 SPAWN-Z-END              BINARY-LONG.
+    01 MIN-DISTANCE             BINARY-LONG.
+    01 PLAYER-INDEX             BINARY-LONG UNSIGNED.
+    *> World data
+    COPY DD-WORLD.
+    COPY DD-CHUNK-REF.
+    *> Player data
+    COPY DD-PLAYERS.
+    COPY DD-SERVER-PROPERTIES.
+LINKAGE SECTION.
+    01 LK-VIEW-DISTANCE         BINARY-LONG UNSIGNED.
+    01 LK-FAILURE               BINARY-CHAR UNSIGNED.
+
+PROCEDURE DIVISION USING LK-VIEW-DISTANCE LK-FAILURE.
+    MOVE 0 TO LK-FAILURE
+
+    *> Do not unload spawn chunks
+    COMPUTE SPAWN-X-START ROUNDED MODE IS TOWARD-LESSER = (WORLD-SPAWN-X / 16) - 1
+    COMPUTE SPAWN-X-END = SPAWN-X-START + 2
+    COMPUTE SPAWN-Z-START ROUNDED MODE IS TOWARD-LESSER = (WORLD-SPAWN-Z / 16) - 1
+    COMPUTE SPAWN-Z-END = SPAWN-Z-START + 2
+
+    PERFORM VARYING CHUNK-INDEX FROM 1 BY 1 UNTIL CHUNK-INDEX > WORLD-CHUNK-COUNT
+        SET ADDRESS OF CHUNK TO WORLD-CHUNK-POINTER(CHUNK-INDEX)
+        IF (CHUNK-X < SPAWN-X-START OR > SPAWN-X-END) OR (CHUNK-Z < SPAWN-Z-START OR > SPAWN-Z-END)
+            COMPUTE CHUNK-BLOCK-X = CHUNK-X * 16 + 8
+            COMPUTE CHUNK-BLOCK-Z = CHUNK-Z * 16 + 8
+            *> Compute the minimum distance to any player on any axis
+            MOVE 1000000 TO MIN-DISTANCE
+            PERFORM VARYING PLAYER-INDEX FROM 1 BY 1 UNTIL PLAYER-INDEX > MAX-PLAYERS
+                IF PLAYER-CLIENT(PLAYER-INDEX) > 0
+                    COMPUTE MIN-DISTANCE = FUNCTION MIN(MIN-DISTANCE, FUNCTION ABS(CHUNK-BLOCK-X - PLAYER-X(PLAYER-INDEX)))
+                    COMPUTE MIN-DISTANCE = FUNCTION MIN(MIN-DISTANCE, FUNCTION ABS(CHUNK-BLOCK-Z - PLAYER-Z(PLAYER-INDEX)))
+                END-IF
+            END-PERFORM
+            *> If the chunk is outside the view distance + 2 (for tolerance against thrashing), unload it
+            COMPUTE MIN-DISTANCE = MIN-DISTANCE / 16 - LK-VIEW-DISTANCE
+            IF MIN-DISTANCE > 2
+                IF CHUNK-DIRTY > 0
+                    CALL "World-SaveChunk" USING CHUNK-INDEX LK-FAILURE
+                    IF LK-FAILURE > 0
+                        MOVE 1 TO LK-FAILURE
+                        GOBACK
+                    END-IF
+                END-IF
+                *> free the memory
+                CALL "World-FreeChunk" USING CHUNK-INDEX
+                *> since the chunk was removed, the next chunk will be at the same index
+                SUBTRACT 1 FROM CHUNK-INDEX
+            END-IF
+        END-IF
+    END-PERFORM
+
+    GOBACK.
+
+END PROGRAM World-UnloadChunks.
---
+++ src/world/world.cob
@@ -0,0 +1,295 @@
+*> --- World-Save ---
+IDENTIFICATION DIVISION.
+PROGRAM-ID. World-Save.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+    COPY DD-WORLD.
+    COPY DD-CHUNK-REF.
+    01 CHUNK-INDEX              BINARY-LONG UNSIGNED.
+LINKAGE SECTION.
+    01 LK-FAILURE               BINARY-CHAR UNSIGNED.
+
+PROCEDURE DIVISION USING LK-FAILURE.
+    *> Create directories. Ignore errors, as they are likely to be caused by the directories already existing.
+    CALL "CBL_CREATE_DIR" USING "save"
+    CALL "CBL_CREATE_DIR" USING "save/region"
+
+    *> Save world metadata
+    CALL "World-SaveLevel" USING LK-FAILURE
+    IF LK-FAILURE > 0
+        GOBACK
+    END-IF
+
+    *> Save dirty chunks
+    PERFORM VARYING CHUNK-INDEX FROM 1 BY 1 UNTIL CHUNK-INDEX > WORLD-CHUNK-COUNT
+        SET ADDRESS OF CHUNK TO WORLD-CHUNK-POINTER(CHUNK-INDEX)
+        IF CHUNK-DIRTY NOT = 0
+            CALL "World-SaveChunk" USING CHUNK-INDEX LK-FAILURE
+            IF LK-FAILURE > 0
+                GOBACK
+            END-IF
+        END-IF
+    END-PERFORM
+
+    GOBACK.
+
+END PROGRAM World-Save.
+
+*> --- World-Load ---
+IDENTIFICATION DIVISION.
+PROGRAM-ID. World-Load.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+    COPY DD-WORLD.
+    01 CHUNK-INDEX              BINARY-LONG UNSIGNED.
+    01 LEVEL-SAVE-REQUIRED      BINARY-CHAR UNSIGNED.
+    01 CHUNK-SAVE-REQUIRED      BINARY-CHAR UNSIGNED.
+LINKAGE SECTION.
+    01 LK-FAILURE               BINARY-CHAR UNSIGNED.
+
+PROCEDURE DIVISION USING LK-FAILURE.
+    MOVE 0 TO LK-FAILURE
+
+    *> Load the world metadata
+    CALL "World-LoadLevel" USING LEVEL-SAVE-REQUIRED
+
+    *> Load just the spawn area
+    PERFORM UNTIL WORLD-CHUNK-COUNT = 0
+        CALL "World-FreeChunk" USING WORLD-CHUNK-COUNT
+    END-PERFORM
+    CALL "World-EnsureSpawnChunks" USING CHUNK-SAVE-REQUIRED
+
+    *> Save the world if necessary
+    IF LEVEL-SAVE-REQUIRED > 0 OR CHUNK-SAVE-REQUIRED > 0
+        CALL "World-Save" USING LK-FAILURE
+    END-IF
+
+    GOBACK.
+
+END PROGRAM World-Load.
+
+*> --- World-Tick ---
+IDENTIFICATION DIVISION.
+PROGRAM-ID. World-Tick.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+    COPY DD-WORLD.
+    COPY DD-CHUNK-REF.
+    COPY DD-CHUNK-ENTITY.
+    COPY DD-PLAYERS.
+    COPY DD-CLIENTS.
+    COPY DD-CLIENT-STATES.
+    COPY DD-SERVER-PROPERTIES.
+    01 PLAYER-INDEX             BINARY-LONG UNSIGNED.
+    01 CHUNK-INDEX              BINARY-LONG UNSIGNED.
+    01 ENTITY-PTR               POINTER.
+    *> Pre-computed player bounding boxes
+    01 PLAYER-AABBS.
+        02 PLAYER-AABB OCCURS PLAYER-CAPACITY TIMES.
+            COPY DD-AABB REPLACING LEADING ==PREFIX== BY ==PLAYER==.
+    01 ENTITY-AABB.
+        COPY DD-AABB REPLACING LEADING ==PREFIX== BY ==ENTITY==.
+    01 COLLISION                BINARY-CHAR UNSIGNED.
+    01 PREVIOUS-ITEM-COUNT      BINARY-LONG UNSIGNED.
+    01 COLLECTED-ITEM-COUNT     BINARY-LONG UNSIGNED.
+    01 CLIENT-ID                BINARY-LONG UNSIGNED.
+    01 BLOCK-POSITION.
+        02 BLOCK-X              BINARY-LONG.
+        02 BLOCK-Y              BINARY-LONG.
+        02 BLOCK-Z              BINARY-LONG.
+    01 BLOCK-ID                 BINARY-LONG.
+    01 REPLACEABLE-PTR          PROGRAM-POINTER.
+    01 REPLACEABLE              BINARY-CHAR UNSIGNED.
+
+PROCEDURE DIVISION.
+    ADD 1 TO WORLD-AGE
+    ADD 1 TO WORLD-TIME
+
+    *> Compute player bounding boxes
+    PERFORM VARYING PLAYER-INDEX FROM 1 BY 1 UNTIL PLAYER-INDEX > MAX-PLAYERS
+        IF PLAYER-CLIENT(PLAYER-INDEX) > 0
+            CALL "PlayerAABB" USING PLAYER-POSITION(PLAYER-INDEX) PLAYER-SNEAKING(PLAYER-INDEX) PLAYER-AABB(PLAYER-INDEX)
+        END-IF
+    END-PERFORM
+
+    *> Tick chunks
+    PERFORM VARYING CHUNK-INDEX FROM 1 BY 1 UNTIL CHUNK-INDEX > WORLD-CHUNK-COUNT
+        SET ADDRESS OF CHUNK TO WORLD-CHUNK-POINTER(CHUNK-INDEX)
+
+        *> Tick entities
+        SET ENTITY-PTR TO CHUNK-ENTITY-LIST
+        PERFORM UNTIL ENTITY-PTR = NULL
+            *> The order here ensures that we have the next-pointer available in case the entity is deallocated
+            SET ADDRESS OF ENTITY-LIST TO ENTITY-PTR
+            SET ENTITY-PTR TO ENTITY-LIST-NEXT
+            PERFORM TickEntity
+        END-PERFORM
+    END-PERFORM
+
+    GOBACK.
+
+TickEntity.
+    ADD 1 TO ENTITY-AGE
+
+    *> TODO This should be specific to item entities
+
+    IF ENTITY-AGE >  6000
+        CALL "World-RemoveEntity" USING ENTITY-LIST-ENTITY
+        EXIT PARAGRAPH
+    END-IF
+
+    *> TODO: Handle block collisions properly... This is just a hack for now.
+    COMPUTE BLOCK-X ROUNDED MODE IS TOWARD-LESSER = ENTITY-X + ENTITY-VELOCITY-X
+    COMPUTE BLOCK-Y ROUNDED MODE IS TOWARD-LESSER = ENTITY-Y + ENTITY-VELOCITY-Y
+    COMPUTE BLOCK-Z ROUNDED MODE IS TOWARD-LESSER = ENTITY-Z + ENTITY-VELOCITY-Z
+    PERFORM CheckBlockCollision
+    IF COLLISION = 0
+        COMPUTE ENTITY-X = ENTITY-X + ENTITY-VELOCITY-X
+        COMPUTE ENTITY-Y = ENTITY-Y + ENTITY-VELOCITY-Y
+        COMPUTE ENTITY-Z = ENTITY-Z + ENTITY-VELOCITY-Z
+
+        COMPUTE ENTITY-VELOCITY-X = ENTITY-VELOCITY-X * 0.98
+        COMPUTE ENTITY-VELOCITY-Y = ENTITY-VELOCITY-Y * 0.98 - 0.04
+        COMPUTE ENTITY-VELOCITY-Z = ENTITY-VELOCITY-Z * 0.98
+
+        PERFORM VARYING CLIENT-ID FROM 1 BY 1 UNTIL CLIENT-ID > MAX-CLIENTS
+            IF CLIENT-PRESENT(CLIENT-ID) = 1 AND CLIENT-STATE(CLIENT-ID) = CLIENT-STATE-PLAY
+                CALL "SendPacket-EntityPositionSync" USING CLIENT-ID ENTITY-ID ENTITY-POSITION ENTITY-ROTATION ENTITY-VELOCITY ENTITY-ON-GROUND
+            END-IF
+        END-PERFORM
+    ELSE
+        MOVE 0 TO ENTITY-VELOCITY-X ENTITY-VELOCITY-Y ENTITY-VELOCITY-Z
+    END-IF
+
+    COMPUTE ENTITY-ITEM-PICKUP-DELAY = FUNCTION MAX(ENTITY-ITEM-PICKUP-DELAY - 1, 0)
+
+    IF ENTITY-ITEM-PICKUP-DELAY <= 0
+        *> Compute the entity AABB, expanded by 1 block horizontally and 0.5 blocks vertically, since the player's pickup
+        *> bounding box is larger than their regular hitbox, but we don't want to recompute each player's AABB for every
+        *> item entity.
+        COMPUTE ENTITY-AABB-MIN-X = ENTITY-X - 0.125 - 1
+        COMPUTE ENTITY-AABB-MAX-X = ENTITY-X + 0.125 + 1
+        COMPUTE ENTITY-AABB-MIN-Y = ENTITY-Y         - 0.5
+        COMPUTE ENTITY-AABB-MAX-Y = ENTITY-Y + 0.25  + 0.5
+        COMPUTE ENTITY-AABB-MIN-Z = ENTITY-Z - 0.125 - 1
+        COMPUTE ENTITY-AABB-MAX-Z = ENTITY-Z + 0.125 + 1
+
+        *> Check for player collisions
+        PERFORM VARYING PLAYER-INDEX FROM 1 BY 1 UNTIL PLAYER-INDEX > MAX-PLAYERS
+            IF PLAYER-CLIENT(PLAYER-INDEX) > 0
+                CALL "CheckCollisionAABB" USING ENTITY-AABB PLAYER-AABB(PLAYER-INDEX) COLLISION
+                IF COLLISION NOT = 0
+                    MOVE ENTITY-ITEM-SLOT-COUNT TO PREVIOUS-ITEM-COUNT
+                    CALL "Inventory-StoreItem" USING PLAYER-INVENTORY(PLAYER-INDEX) ENTITY-ITEM-SLOT
+                    IF ENTITY-ITEM-SLOT-COUNT < PREVIOUS-ITEM-COUNT
+                        COMPUTE COLLECTED-ITEM-COUNT = ENTITY-ITEM-SLOT-COUNT - PREVIOUS-ITEM-COUNT
+                        PERFORM SendPickupItem
+                        *> TODO sync just the slot that changed
+                        CALL "Inventory-SyncPlayerInventory" USING PLAYER-INDEX
+                    END-IF
+                    IF ENTITY-ITEM-SLOT-COUNT < 1
+                        PERFORM SendPickupItem
+                        CALL "World-RemoveEntity" USING ENTITY-LIST-ENTITY
+                        EXIT PARAGRAPH
+                    END-IF
+                END-IF
+            END-IF
+        END-PERFORM
+    END-IF
+    .
+
+SendPickupItem.
+    PERFORM VARYING CLIENT-ID FROM 1 BY 1 UNTIL CLIENT-ID > MAX-CLIENTS
+        IF CLIENT-PRESENT(CLIENT-ID) NOT = 0 AND CLIENT-STATE(CLIENT-ID) = CLIENT-STATE-PLAY
+            CALL "SendPacket-TakeItemEntity" USING CLIENT-ID ENTITY-ID PLAYER-INDEX COLLECTED-ITEM-COUNT
+        END-IF
+    END-PERFORM
+    .
+
+CheckBlockCollision.
+    CALL "World-GetBlock" USING BLOCK-POSITION BLOCK-ID
+    IF BLOCK-ID <= 0
+        MOVE 0 TO COLLISION
+        EXIT PARAGRAPH
+    END-IF
+
+    CALL "GetCallback-BlockReplaceable" USING BLOCK-ID REPLACEABLE-PTR
+    IF REPLACEABLE-PTR = NULL
+        MOVE 0 TO COLLISION
+        EXIT PARAGRAPH
+    END-IF
+
+    CALL REPLACEABLE-PTR USING BLOCK-POSITION REPLACEABLE
+    IF REPLACEABLE = 0
+        MOVE 1 TO COLLISION
+    ELSE
+        MOVE 0 TO COLLISION
+    END-IF
+    .
+
+END PROGRAM World-Tick.
+
+*> --- World-FindSpawnLocation ---
+*> This program is used to find a suitable spawn location for a new player.
+IDENTIFICATION DIVISION.
+PROGRAM-ID. World-FindSpawnLocation.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+    78 SPAWN-RADIUS             VALUE 10.
+    78 SPAWN-ATTEMPTS           VALUE 100.
+    COPY DD-WORLD.
+    01 BLOCK-POSITION.
+        02 POS-X                BINARY-LONG.
+        02 POS-Y                BINARY-LONG.
+        02 POS-Z                BINARY-LONG.
+    01 RANDOM-VALUE             FLOAT-LONG                  VALUE 0.
+    01 BLOCK-STATE-ID           BINARY-LONG.
+    01 BLOCK-IDENTIFIER         PIC X(256).
+    01 AIR-COUNT                BINARY-LONG UNSIGNED.
+LINKAGE SECTION.
+    01 LK-POSITION.
+        02 LK-X                 FLOAT-LONG.
+        02 LK-Y                 FLOAT-LONG.
+        02 LK-Z                 FLOAT-LONG.
+
+PROCEDURE DIVISION USING LK-POSITION.
+    IF RANDOM-VALUE = 0
+        MOVE FUNCTION RANDOM(FUNCTION CURRENT-DATE(1:16)) TO RANDOM-VALUE
+    END-IF
+
+    MOVE WORLD-SPAWN-X TO POS-X LK-X
+    MOVE WORLD-SPAWN-Y TO POS-Y LK-Y
+    MOVE WORLD-SPAWN-Z TO POS-Z LK-Z
+
+    PERFORM SPAWN-ATTEMPTS TIMES
+        MOVE FUNCTION RANDOM TO RANDOM-VALUE
+        COMPUTE POS-X = WORLD-SPAWN-X + (RANDOM-VALUE - 0.5) * SPAWN-RADIUS
+        MOVE FUNCTION RANDOM TO RANDOM-VALUE
+        COMPUTE POS-Z = WORLD-SPAWN-Z + (RANDOM-VALUE - 0.5) * SPAWN-RADIUS
+
+        MOVE 0 TO AIR-COUNT
+        PERFORM VARYING POS-Y FROM 319 BY -1 UNTIL POS-Y < -63
+            CALL "World-GetBlock" USING BLOCK-POSITION BLOCK-STATE-ID
+            CALL "Blocks-Get-Name" USING BLOCK-STATE-ID BLOCK-IDENTIFIER
+            EVALUATE BLOCK-IDENTIFIER
+                WHEN = "minecraft:air" OR = "minecraft:cave_air" OR = "minecraft:void_air"
+                    ADD 1 TO AIR-COUNT
+                WHEN OTHER
+                    IF AIR-COUNT >= 2
+                        COMPUTE LK-X = POS-X + 0.5
+                        COMPUTE LK-Y = POS-Y + 1
+                        COMPUTE LK-Z = POS-Z + 0.5
+                        GOBACK
+                    END-IF
+                    MOVE 0 TO AIR-COUNT
+            END-EVALUATE
+        END-PERFORM
+    END-PERFORM
+
+    GOBACK.
+
+END PROGRAM World-FindSpawnLocation.
